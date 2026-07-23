cd9d71bc3c0b52de1c393e0a07cfa28bab0ab3a5
charan sc <charansc328@gmail.com>
2026-07-23 22:01:47 +0530

    Add files via upload

---
+++ Task03_Datatypes.js
@@ -0,0 +1,7 @@
+let patientAge = 45;
+let patientName = "John Doe";
+let isAdmitted = true;
+
+console.log("patientAge:", patientAge, "| Type:", typeof patientAge);
+console.log("patientName:", patientName, "| Type:", typeof patientName);
+console.log("isAdmitted:", isAdmitted, "| Type:", typeof isAdmitted);
