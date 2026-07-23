dd4b3a30ea441d5a42d46fc2c9152588a748df57
charan sc <charansc328@gmail.com>
2026-07-23 22:02:15 +0530

    Add files via upload

---
+++ Task05_IfElse.js
@@ -0,0 +1,9 @@
+let patientAge = 16;
+
+console.log("Patient Age:", patientAge);
+
+if (patientAge >= 18) {
+    console.log("Patient Category: Adult");
+} else {
+    console.log("Patient Category: Minor");
+}
