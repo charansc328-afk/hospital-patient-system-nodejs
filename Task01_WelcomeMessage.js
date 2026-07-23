1ba62e59809c0d703be55985edf7928369e5038f
charan sc <charansc328@gmail.com>
2026-07-23 22:00:29 +0530

    Add welcome message to Task01_WelcomeMessage.js

---
+++ Task01_WelcomeMessage.js
@@ -0,0 +1,4 @@
+console.log("=========================================");
+console.log("   Welcome to Hospital Patient System    ");
+console.log("=========================================");
+console.log("Managing patient records with care.");
