d741f92d91c284aa01f69b78e97ecf6e14dca9fd
charan sc <charansc328@gmail.com>
2026-07-23 22:01:30 +0530

    Add files via upload

---
+++ Task02_Variables.js
@@ -0,0 +1,9 @@
+let systemName = "Hospital Patient System";
+let totalPatients = 120;
+
+console.log("System Name:", systemName);
+console.log("Total Patients:", totalPatients);
+
+totalPatients = 125;
+
+console.log("Updated Total Patients:", totalPatients);
