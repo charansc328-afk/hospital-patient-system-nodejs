73572c0d641481d532020c22de193c2f8e716c47
charan sc <charansc328@gmail.com>
2026-07-23 22:03:28 +0530

    Add files via upload

---
+++ Task10_PatientObject.js
@@ -0,0 +1,13 @@
+let patient = {
+    id: 101,
+    name: "Sarah Johnson",
+    age: 52,
+    ward: "Oncology",
+    isAdmitted: true
+};
+
+console.log("Patient ID:", patient.id);
+console.log("Patient Name:", patient.name);
+console.log("Patient Age:", patient.age);
+console.log("Patient Ward:", patient.ward);
+console.log("Is Admitted:", patient.isAdmitted);
