9f5c17a34d347eef89cf10eead11d9fb08008cf8
charan sc <charansc328@gmail.com>
2026-07-23 22:02:02 +0530

    Add files via upload

---
+++ Task04_AgeDifference.js
@@ -0,0 +1,8 @@
+let patientOneAge = 65;
+let patientTwoAge = 38;
+
+let ageDifference = patientOneAge - patientTwoAge;
+
+console.log("Patient One Age:", patientOneAge);
+console.log("Patient Two Age:", patientTwoAge);
+console.log("Age Difference:", ageDifference, "years");
